6c916dbe5b645295abe25086cff40651f13d4f37
Sam <samuelpulukuri@outlook.com>
2023-10-30 19:00:47 +0530

    Add cells manually and arrange them with just the script

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const container = document.querySelector(".container");
+
+for (let i = 1; i <= 256; i++) {
+  const cell = document.createElement("div");
+  cell.className = "cell";
+  container.appendChild(cell);
+}
